aed92350aa129ebab34cc301a0e5d87cce527fad
Elzara Turakulova <endjik@gmail.com>
2024-10-29 09:47:02 -0700

    New version with read all

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from .routes.cat_routes import cats_bp
+from .db import migrate, db
+from .models import cat
 
 def create_app():
-    # __name__ stores the name of the module we're in
+
     app = Flask(__name__)
+    # __name__ stores the name of the module we're in
+    app.config['SQLALCHEMY_TRACK_NOTIFICATION'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        'postgresql+psycopg2://elzara:postgres@localhost:5432/flasky_development'
+    )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(cats_bp)
 
--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,18 +1,69 @@
-from flask import Blueprint
-from ..models.cat import cats
+from flask import Blueprint, abort, make_response, request
+from ..db import db
+from app.models.cat import Cat
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
 
+@cats_bp.post("")
+def create_cat():
+    request_body = request.get_json()
+    name = request_body["name"]
+    color = request_body["color"]
+    personality = request_body["personality"]
+
+
+    new_cat = Cat(name=name, color=color, personality=personality)
+    db.session.add(new_cat)
+    db.session.commit()
+
+    response = {
+        "id": new_cat.id,
+        "name": new_cat.name,
+        "color": new_cat.color,
+        "personality": new_cat.personality,
+    }
+    return response, 201
+
 @cats_bp.get("")
 def get_all_cats():
-    results_list = []
+    query = db.select("Cat").order_by("Cat.id")
+    cats = db.session.scalars(query)
 
-    for cat in cats:
-        results_list.append(dict(
-            id=cat.id,
-            name=cat.name,
-            color=cat.color,
-            personality=cat.personality
-        ))
+    cats_response = [cat.to_dict() for cat in cats]
+    return cats_response
+
+
+
     
-    return results_list
+# @cats_bp.get("")
+# def get_all_cats():
+#     results_list = []
+
+#     for cat in cats:
+#         results_list.append(dict(
+#             id=cat.id,
+#             name=cat.name,
+#             color=cat.color,
+#             personality=cat.personality
+#         ))
+
+#     return results_list
+
+# def validate_cat(cat_id):
+#     try:
+#         cat_id = int(cat_id)
+#     except:
+#         response = {"message": f"cat {cat_id} invalid"}
+#         abort(make_response(response, 400))
+
+#         for cat in cats:
+#             if cat.id == cat_id:
+#                 return cat
+
+#     response = {"message": f"cat {cat_id} not found"}
+#     abort(make_response(response, 404))
+
+# @cats_bp.get("/<cat_id>")
+# def get_one_cat(cat_id):
+#     cat = validate_cat(cat_id)
+#     return cat.to_dict(), 200
